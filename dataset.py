76209d525fed1d262dd00bfe18534ec7ae937b41
HuaTanSang <huatansang2004@gmail.com>
2025-03-21 22:39:16 +0700

    update dataset

--- dataset.py
+++ dataset.py
@@ -1,11 +1,15 @@
 import torch 
+import pandas as pd 
+
 import torch.nn as nn 
 import torch.nn.functional as F 
 from torch.utils.data import Dataset
 
 class UIT_VFSC(Dataset): 
-    def __init__(self, folder_dir): 
+    def __init__(self, file_dir): 
         super().__init__() 
         
-        self.folder_dir = folder_dir
+        self.folder_dir = file_dir
+
+
         
